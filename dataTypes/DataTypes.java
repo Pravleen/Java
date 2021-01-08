1ee6d74c63717034bd38a31eaebf98cfbc6786c6
Pravleen <47741189+Pravleen@users.noreply.github.com>
2021-01-08 19:28:21 +0530

    Add files via upload

---
+++ dataTypes/DataTypes.java
@@ -0,0 +1,21 @@
+package dataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte x=2;
+		System.out.println(x);
+		int age =10;
+		System.out.println(age);
+		float rateOfInterest=2.3f;
+		double rate=2.345;
+		System.out.println(rateOfInterest);
+		System.out.println(rate);
+		boolean isItCool=true;
+		System.out.println(isItCool);
+		char initial='p';
+		System.out.print(initial);
+	}
+
+}
