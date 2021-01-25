31967aa4dd44638cb5e730a0ce0a537034ff3ab9
Pravleen <47741189+Pravleen@users.noreply.github.com>
2021-01-25 23:06:32 +0530

    Add files via upload

---
+++ encapsulation/EncapsulationIntro.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class EncapsulationIntro {
+
+	public static void main(String[] args) {
+		Student obj =new Student();
+		obj.setAge(12);
+		System.out.println("Age is"+obj.getAge());
+
+	}
+
+}
