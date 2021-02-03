abe132f4d2fdf13b6651b58b2cf02f88312e5767
Pravleen <47741189+Pravleen@users.noreply.github.com>
2021-02-03 23:31:16 +0530

    Add files via upload

---
+++ singletons/MainClass.java
@@ -0,0 +1,12 @@
+package singletons;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		AppConfig a=new App AppConfig();
+		AppConfig obj=AppConfig.getInstance();
+
+	}
+
+}
