393e75808f08e6960eff5ff383a278e34bbe0dbd
Pravleen <47741189+Pravleen@users.noreply.github.com>
2021-02-05 22:40:08 +0530

    Add files via upload

---
+++ oops/Abstraction/WagonR.java
@@ -0,0 +1,17 @@
+package oops.Abstraction;
+
+public class WagonR extends Car {
+	
+	@Override
+	public void brake() {
+		System.out.println("Brake pressed WagonR");
+	}
+
+	@Override
+	public void accelerate() {
+		System.out.println("WaganR is accelerating ");
+		// TODO Auto-generated method stub
+		
+	}
+
+}
